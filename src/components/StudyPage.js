243529a17de12cd357264e6e54daf4c911789273
Matthew <morch028@uottawa.ca>
2020-02-22 21:25:28 -0500

    Shows entity form when entity selected

--- src/components/StudyPage.js
+++ src/components/StudyPage.js
@@ -1,17 +1,29 @@
-import "./StudyPage.css";
 import EntityIndex from "./EntityIndex";
 import EntityTimeline from "./EntityTimeline";
+import { useState } from "preact/hooks";
+import "./StudyPage.css";
+import EntityForm from "./EntityForm";
 
 const StudyPage = ({ entities, startYear, endYear }) => {
+  const [selectedEntity, setSelectedEntity] = useState(null);
+  const handleOnSelect = entityId =>
+    setSelectedEntity(entities.find(entity => entity.id === entityId));
+
   return (
     <main className="study-page">
+      {selectedEntity && (
+        <EntityForm
+          selectedEntity={selectedEntity}
+          onRequestClose={() => setSelectedEntity(null)}
+        />
+      )}
       <EntityTimeline
         startYear={startYear}
         endYear={endYear}
         entities={entities}
-        onSelect={console.log}
+        onSelect={handleOnSelect}
       />
-      <EntityIndex entities={entities} onSelect={console.log} />
+      <EntityIndex entities={entities} onSelect={handleOnSelect} />
     </main>
   );
 };

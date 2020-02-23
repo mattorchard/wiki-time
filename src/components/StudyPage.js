384e3b44e093f73445f1cf880a853b1731cb191d
Matthew <morch028@uottawa.ca>
2020-02-22 21:20:06 -0500

    Moves media query into CSS

--- src/components/StudyPage.js
+++ src/components/StudyPage.js
@@ -1,15 +1,10 @@
 import "./StudyPage.css";
 import EntityIndex from "./EntityIndex";
-import useMediaQuery from "../hooks/useMediaQuery";
 import EntityTimeline from "./EntityTimeline";
 
 const StudyPage = ({ entities, startYear, endYear }) => {
-  const sideBySide = useMediaQuery("(min-width: 1000px)");
   return (
-    <main
-      className="study-page"
-      style={{ "--index-width": sideBySide ? "25vw" : "100vw" }}
-    >
+    <main className="study-page">
       <EntityTimeline
         startYear={startYear}
         endYear={endYear}
